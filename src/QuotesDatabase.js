50944b922933397422cb835611f08c547b5eaffe
TejalKhade28 <79683880+TejalKhade28@users.noreply.github.com>
2021-04-11 17:11:11 +0530

    Add files via upload

---
+++ src/QuotesDatabase.js
@@ -0,0 +1,235 @@
+const quotes=[
+	{
+    "quote": "If you want to shine like a sun, first burn like a sun.",
+    "author": "Dr.APJ Abdul Kalam"
+    }, {
+    "quote": "I have been constantly telling people to encourage people, to question the unquestioned and not to be ashamed to bring up new ideas, new processes to get things done.",
+    "author": "Ratan Tata"
+    }, {
+    "quote": "A house divided against itself cannot stand.",
+    "author": "Abraham Lincoln"
+    }, {
+    "quote": "Difficulties increase the nearer we get to the goal.",
+    "author": "Johann Wolfgang von Goethe"
+    }, {
+    "quote": "Fate is in your hands and no one elses",
+    "author": "Byron Pulsifer"
+    }, {
+    "quote": "Man needs difficulties in life because they are necessary to enjoy success.",
+    "author": "Dr.APJ Abdul Kalam"
+	}, {
+	"quote": "Talk to yourself once in a day, otherwise you may miss meeting an intelligent person in this world.",
+	"author": "Swami Vivekananda"
+	}, {
+	"quote": "Well begun is half done.",
+	"author": "Aristotle"
+	}, {
+	"quote": "Ups and downs in life are very important to keep us going, because a straight line even in an ECG means we are not alive.",
+	"author": "Ratan Tata"
+	}, {
+	"quote": "Self-complacency is fatal to progress.",
+	"author": "Margaret Sangster"
+	}, {
+	"quote": "The greatest religion is to be true to your own nature. Have faith in yourselves.",
+	"author": "Swami Vivekananda"
+	}, {
+	"quote": "Be dependent on yourself, and know that courage has to be born within you. It takes time but you have to work for it.",
+    "author": "Sudha Murthy"
+	}, {
+	"quote": "It’s fine to celebrate success but it is more important to heed the lessons of failure.",
+	"author": "Bill Gates"
+	}, {
+	"quote": "Life is change. Growth is optional. Choose wisely.",
+	"author": "Karen Clark"
+	}, {
+	"quote": "You'll see it when you believe it.",
+	"author": "Wayne Dyer"
+	}, {
+	"quote": "Don’t compare yourself with anyone in this world…if you do so, you are insulting yourself.",
+	"author": "Bill Gates"
+	}, {
+	"quote": "I do not only rely on my technique or strength but also on my mind.",
+	"author": "Mary Kom"
+	}, {
+	"quote": "All our dreams can come true, if we have the courage to pursue them",
+	"author": "Walt Disney-American Filmmaker"
+	}, {
+	"quote": "To lead people walk behind them.",
+	"author": "Lao Tzu"
+	}, {
+	"quote": "Having nothing, nothing can he lose.",
+	"author": "William Shakespeare"
+	}, {
+	"quote": "Setting goals is the first step in turning the invisible into the visible.",
+	"author": "Tony Robbins-American Bussinessman"
+	}, {
+	"quote": "A rolling stone gathers no moss.",
+	"author": "Publilius Syrus"
+	}, {
+	"quote": "Ideas are the beginning points of all fortunes.",
+	"author": "Napoleon Hill"
+	}, {
+	"quote": "He can who thinks he can, and he can't who thinks he can't. This is an inexorable, indisputable law.",
+	"author": "Pablo Picasso"
+	}, {
+	"quote": "When you want something, all the universe conspires in helping you to achieve it.",
+	"author": "Paulo Coelho"
+	}, {
+	"quote": "Trust yourself. You know more than you think you do.",
+	"author": "Benjamin Spock"
+	}, {
+	"quote": "Victory is not always winning the battle...but rising every time you fall.",
+	"author": "Napoleon Bonaparte - French commander"
+	}, {
+	"quote": "It is not in the stars to hold our destiny but in ourselves.",
+	"author": "William Shakespeare"
+	}, {
+	"quote": "From error to error one discovers the entire truth.",
+	"author": "Sigmund Freud"
+	},{
+	"quote": "Well done is better than well said.",
+	"author": "Benjamin Franklin"
+	}, {
+	"quote": "It is our choices that show what we truly are, far more than our abilities.",
+	"author": "J.K. Rowling"
+	}, {
+	"quote": "Work out your own salvation. Do not depend on others.",
+	"author": "Buddha"
+	}, {
+	"quote": "One today is worth two tomorrows.",
+	"author": "Benjamin Franklin"
+	}, {
+	"quote": "Once you choose hope, anythings possible.",
+	"author": "Christopher Reeve"
+	}, {
+	"quote": "God always takes the simplest way.",
+	"author": "Albert Einstein"
+	}, {
+	"quote": "Shoot for the moon. Even if you miss, you'll land among the stars. ",
+	"author": "Les Brown"
+	}, {
+	"quote": "Keep your face always toward the sunshine—and shadows will fall behind you",
+	"author": "Walt Whitman – American poet & journalist"
+	}, {
+	"quote": "Learning is a treasure that will follow its owner everywhere",
+	"author": "Chinese proverb"
+	}, {
+	"quote": "Be as you wish to seem.",
+	"author": "Socrates"
+	}, {
+	"quote": "The world is always in movement.",
+	"author": "V. Naipaul"
+	}, {
+	"quote": "Opportunities don't happen. You create them.",
+	"author": "Chris Grosser"
+	}, {
+	"quote": "What worries you masters you.",
+	"author": "Haddon Robinson"
+	}, {
+	"quote": "One faces the future with ones past.",
+	"author": "Pearl Buck"
+	}, {
+	"quote": "Goals are the fuel in the furnace of achievement.",
+	"author": "Brian Tracy"
+	}, {
+	"quote": "Who sows virtue reaps honour.",
+	"author": "Leonardo da Vinci"
+	}, {
+	"quote": "Be kind whenever possible. It is always possible.",
+	"author": "Dalai Lama"
+	}, {
+	"quote": "Great minds discuss ideas; average minds discuss events; small minds discuss people.",
+	"author": "Eleanor Roosevelt"
+	}, {
+	"quote": "He is able who thinks he is able.",
+	"author": "Buddha"
+	}, {
+	"quote": "Be as you wish to seem.",
+	"author": "Socrates"
+	}, {
+	"quote": "A goal without a plan is just a wish.",
+	"author": "Larry Elder"
+	}, {
+	"quote": "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+	"author": "Mahatma Gandhi"
+	}, {
+	"quote": "Learn from yesterday, live for today, hope for tomorrow.",
+	"author": "Albert Einstein"
+	}, {
+	"quote": "I have not failed. I've just found 10,000 ways that won't work.",
+	"author": "Thomas Edison"
+	}, {
+	"quote": "All serious daring starts from within.",
+	"author": "Harriet Beecher Stowe"
+	}, {
+	"quote": "The best teacher is experience learned from failures.",
+	"author": "Byron Pulsifer"
+	}, {
+	"quote": "Think how hard physics would be if particles could think.",
+	"author": "Murray Gell-Mann"
+	},{
+	"quote": "One fails forward toward success.",
+	"author": "Charles Kettering"
+	}, {
+	"quote": "Time is the wisest counsellor of all.",
+	"author": "Pericles"
+	}, {
+	"quote": "You give before you get.",
+	"author": "Napoleon Hill"
+	}, {
+	"quote": "Wisdom begins in wonder.",
+	"author": "Socrates"
+	}, {
+	"quote": "Without courage, wisdom bears no fruit.",
+	"author": "Baltasar Gracian"
+	}, {
+	"quote": "Change in all things is sweet.",
+	"author": "Aristotle"
+	}, {
+	"quote": "What you fear is that which requires action to overcome.",
+	"author": "Byron Pulsifer"
+	}, {
+	"quote": "The best teacher is experience learned from failures.",
+	"author": "Byron Pulsifer"
+	}, {
+	"quote": "When performance exceeds ambition, the overlap is called success.",
+	"author": "Cullen Hightower"
+	},{
+	"quote": "Real magic in relationships means an absence of judgement of others.",
+	"author": "Wayne Dyer"
+	}, {
+	"quote": "When performance exceeds ambition, the overlap is called success.",
+	"author": "Cullen Hightower"
+	}, {
+	"quote": "I never think of the future. It comes soon enough.",
+	"author": "Albert Einstein"
+	},{
+	"quote": "I believe that every person is born with talent.",
+	"author": "Maya Angelou"
+	}, {
+	"quote": "Important principles may, and must, be inflexible.",
+	"author": "Abraham Lincoln"
+	}, {
+	"quote": "The undertaking of a new action brings new strength.",
+	"author": "Richard Evans"
+	}, {
+	"quote": "I believe that every person is born with talent.",
+	"author": "Maya Angelou"
+	}, {
+	"quote": "Great talent finds happiness in execution.",
+	"author": "Johann Wolfgang von Goethe"
+	}, {
+	"quote": "Faith in oneself is the best and safest course.",
+	"author": "Michelangelo"
+	}, {
+	"quote": "Courage is going from failure to failure without losing enthusiasm.",
+	"author": "Winston Churchill"
+	}, {
+	"quote": "The two most powerful warriors are patience and time.",
+	"author": "Leo Tolstoy"
+	}, {
+	"quote": "Anticipate the difficult by managing the easy.",
+	"author": "Lao Tzu"
+	},
+]
+export default quotes
